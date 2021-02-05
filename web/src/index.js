90efbc6c0deeb90e9f38ec1f6b725811b506b809
Daniil <daniil@pas>
2021-02-05 12:05:40 +0300

    suppress log

--- web/src/index.js
+++ web/src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/app/App';
 
+// suppress log
+if (process.env.NODE_ENV !== 'development') {
+  console.log = () => {}
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
