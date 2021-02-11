c5c8aeab4f1eadf596a4c009212f8ec9f8415c10
Daniil <daniil@pas>
2021-02-12 01:50:53 +0300

    ver 1.3.0

--- web/src/components/app/App.js
+++ web/src/components/app/App.js
@@ -23,7 +23,7 @@ export default class App extends React.Component {
       <Header />
       <Editor />
       <NotesRenderer />
-      <Version value="1.2.0"/>      
+      <Version value="1.3.0"/>      
     </div>
   }
 }
